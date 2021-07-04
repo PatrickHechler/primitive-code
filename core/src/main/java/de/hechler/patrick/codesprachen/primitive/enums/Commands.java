f4ebaefecd687e8ca03108d312dc76c13d669d11
Patrick <Patrick@Coruscant.Speedport_W_724V_01011603_07_002>
2021-07-04 20:10:14 +0200

    refactor to three projects

---
+++ core/src/main/java/de/hechler/patrick/codesprachen/primitive/enums/Commands.java
@@ -0,0 +1,32 @@
+package de.hechler.patrick.codesprachen.primitive.enums;
+
+
+public enum Commands {
+	
+	add, sub, mul, div,
+	
+	neg,
+	
+	
+	and, or, xor,
+	
+	not,
+	
+	
+	
+	push, pop,
+	
+	
+	
+	cmp,
+	
+	
+	jmp, jmpeq, jmpne, jmpgt, jmpge, jmplo, jmple,
+	
+	
+	call, calleq, callne, callgt, callge, calllo, callle,
+	
+	
+	label,
+
+}
